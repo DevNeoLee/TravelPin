b905b5c9464c8a59f0c7d3ade27bb6d3f5d3ef3c
DevNeoLee <devneolee@gmail.com>
2020-03-12 00:13:30 -0700

    Update assets images

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
         @user = User.new(user_params)
 
         if @user.save
-            redirect_to user_path(@user), notice: " Thank you for Signing Up! : )/n Now, Please Log In" 
+            redirect_to user_path(@user), notice: " Thank you! Now, Please Log In : )" 
         else
             render 'new'
         end
