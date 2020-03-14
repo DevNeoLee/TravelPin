6125424f06a46d109cb436305a737aaf2ea6381b
DevNeoLee <devneolee@gmail.com>
2020-03-13 23:22:18 -0700

    Update cors.json

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ Bundler.require(*Rails.groups)
 module Travelpin
   class Application < Rails::Application
     
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
